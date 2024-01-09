ecb6b6cce7d42cfabbad569e16e3684b33b147ef
Arhivar <artyr25072001@gmail.com>
2024-01-09 15:55:47 -0500

    1

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function getLastElementMeta(array) {
+  const lastElementIndex = array.length - 1;
+  return console.log(array.length, array.length - 1);
+}
+getLastElementMeta(["apple", "peach", "pear"]);
