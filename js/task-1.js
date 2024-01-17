4eabc7012f931ff1ea8e0af368a61c5553fe55ac
Arhivar <artyr25072001@gmail.com>
2024-01-17 14:52:17 -0500

    1

--- js/task-1.js
+++ js/task-1.js
@@ -6,7 +6,7 @@ function slugify(title) {
   const resultSlugify = splitSlugify.join("-");
   console.log(resultSlugify);
 }
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+slugify("Arrays for begginers");
+slugify("English for developer");
+slugify("Ten secrets of JavaScript");
+slugify("How to become a JUNIOR developer in TWO WEEKS");
